808de9f11adca98ed511b670d5625ec7a47db0bd
D.Zou <Dawenzou1@gmail.com>
2016-04-17 19:13:19 -0400

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -4,6 +4,6 @@ from cv2 import imwrite
 
 img = cv2.imread('C:\Users\Dawen\Desktop\code\green.png')
 res = cv2.resize(img,None,None,fx=2, fy=2,interpolation = cv2.INTER_AREA)
-cv2.imwrite('green_resize.png',res)
+cv2.imwrite('green_resized.png',res)
 
 ##read-resize-write: success
