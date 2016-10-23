04672ccb5e6922ffcdf1323a89a7bb0cb31bec42
D.Zou <Dawenzou1@gmail.com>
2016-10-22 21:07:07 -0400

    Add files via upload

---
+++ TCP Server.py
@@ -0,0 +1,27 @@
+from socket import *
+
+lskt = socket(AF_INET, SOCK_STREAM)
+lskt.bind(('localhost',1400))
+
+## listens for a connection
+lskt.listen(SOMAXCONN)
+
+print "listening"
+
+while True:
+	cskt, client_address = lskt.accept()
+	print str(client_address)
+
+	## wait to recieve message/data
+	msg = cskt.recv(2048)
+	print "message: " + str(msg)
+
+	cskt.sendall(msg)
+	## redirect message to sender
+	lskt.sendall(str(msg))
+
+cskt.close()
+
+##  this might be the right way of doing it
+## http://stackoverflow.com/questions/9852045/how-to-make-a-simple-proxy-in-python
+
