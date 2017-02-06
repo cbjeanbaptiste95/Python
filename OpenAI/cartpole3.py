9e427b76d4c128e28a9f77093597c80fd5733074
D.Zou <Dawenzou1@gmail.com>
2017-02-06 15:30:44 -0500

    Add files via upload

--- OpenAI/cartpole3.py
+++ OpenAI/cartpole3.py
@@ -26,22 +26,24 @@ import time
 def episode():
     env = gym.make('CartPole-v0')
     observation = env.reset()
-    parameters = np.random.rand(4) * 2 - 1                         #[-1,1]
+    parameters = np.random.rand(4) * 2 - 1                         #generates 4 random numbers, in the range of 2, minus 1: range {-1,1}
     actions = 0;
-    totalreward = 0
+    score = 0
     rounds = 0
-    for ep in range (20):                                             #plays 20 rounds
+    for ep in range (40):                                             #plays 20 rounds
         rounds += 1                                                   #increment rounds
         observation = env.reset()                                     #reset the game
         actions = 0                                                   #reset the action count
+        totalreward = 0                                               #reset rewards
         for _ in xrange(100000):
             env.render()
             action = 0 if np.matmul(observation,parameters) < 0 else 1 #{0 or 1} by multiplying the matrix
             actions += 1                                               #increment actions
-            observation, reward, done, info = env.step(action)         #perform action
-            totalreward += reward                                      #weight
+            observation, reward, done, info = env.step(action)         #perform action                                        #weight
+                                                                       #rewards are used to feed an agents compulsion loop
             time.sleep(0.04)                                           #slow down
             print observation                                          #print obs data
+
             if done:                                                   #if done == true
                 print("game ended after ", actions, " actions")        #show action count
                 break
@@ -49,3 +51,10 @@ def episode():
         print(rounds)
 
 episode()
+"""
+    https://www.youtube.com/watch?v=IMwh7JTcEsg
+    https://www.youtube.com/watch?v=IK7l4ZLm55I
+
+    author of the video details why its important to have a reward system to
+    incentivize an intelligent agent
+"""
